086f0a77dd7bf9264b3a5000204044bf19684ba4
LSG2001 <luca.gemballa@gmx.de>
2020-11-07 11:42:16 +0100

    Added fuck

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main (String[] args) {
         System.out.println("hello");
+        System.out.println("fuck");
         camara a = new camara();
 
     }
